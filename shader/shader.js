63f5ed6fcb4af78ce7f37e0909a0196192b65678
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2024-05-28 20:42:11 +0000

    gb color

--- shader/shader.js
+++ shader/shader.js
@@ -13,14 +13,20 @@ const frag = glsl(/* glsl */`
   precision highp float;
 
   uniform float time;
+  uniform float aspect;
+
   varying vec2 vUv;
 
   void main () {
     vec3 colorA = vec3(1.0, 0.0, 0.0);
     vec3 colorB = vec3(0.0, 1.0, 0.0);
 
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+    float dist = length(center);
+
     vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, dist > 0.25 ? 0.0 : 1.0);
   }
 `);
 
@@ -28,6 +34,8 @@ const frag = glsl(/* glsl */`
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
+    // clearColor: 'false', // this will set the background color to transparent
+    clearColor: 'white',
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
@@ -35,7 +43,8 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time
+      time: ({ time }) => time,
+      aspect: ({ width, height }) => width / height
     }
   });
 };
