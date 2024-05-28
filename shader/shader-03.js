3d1a4458751ca222d52edb2ebc566804dd4ace38
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2024-05-28 20:31:47 +0000

    shadder and glsl

--- shader/shader-03.js
+++ shader/shader-03.js
@@ -16,11 +16,7 @@ const frag = glsl(/* glsl */`
   varying vec2 vUv;
 
   void main () {
-    vec3 colorA = vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.0);
-
-    vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(vec3(vUv.y), 1.0);
   }
 `);
 
--- shader/shader-04.js
+++ shader/shader-04.js
@@ -16,10 +16,7 @@ const frag = glsl(/* glsl */`
   varying vec2 vUv;
 
   void main () {
-    vec3 colorA = vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.0);
-
-    vec3 color = mix(colorA, colorB, vUv.x);
+    vec3 color = vec3(sin(time) + 1.0);
     gl_FragColor = vec4(color, 1.0);
   }
 `);
